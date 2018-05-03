64826018bfdef3975a821c8091c67b3918a656cb
Gerald S. Brown <gbrown@gerbreown.com>
2018-05-03 09:50:43 +0800

    Changes from pull 2

--- medrec/settings.py
+++ medrec/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'nf^(#gts_4-vfd74(3i-hv8qcnq&cv-9ozi-l+&j@oeb4)z3k%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.118', '192.168.0.121', '192.168.0.122','127.0.0.1', 'localhost']
 
+ALLOWED_HOSTS = ['192.168.0.119', '192.168.0.121', '192.168.0.122','127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -45,11 +45,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'easy_pdf',
     'rest_framework',
-    
-   
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+	'djdev_panel.middleware.DebugMiddleware',
+	'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,8 +58,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     ]
 MIDDLEWARE += ('crum.CurrentRequestUserMiddleware',)
+
+if DEBUG:
+	INTERNAL_IPS = ('127.0.0.1', 'localhosr',)
+	MIDDLEWARE += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+
 
 ROOT_URLCONF = 'medrec.urls'
 
@@ -80,16 +87,34 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'medrec.wsgi.application'
 
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
+
+DEBUG_TOOLBAR_CONFIG = {
+	'INTERCEPT:REDIRECTS': False,}
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.mysql',
-        'ENGINE': 'mysql.connector.django',
+        'ENGINE': 'django.db.backends.mysql',
+        #'ENGINE': 'mysql.connector.django',
         'NAME': 'medrec',
-        'USER':  'root',
+        'USER':  'medrec',
         'PASSWORD': 'M3dR3c#123',
         'HOST':  'localhost',
         'PORT': '3306',
